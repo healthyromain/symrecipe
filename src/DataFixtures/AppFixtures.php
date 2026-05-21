6eb95a0da7196aa79012850a00846a6c632b9320
healtyromain <romainlecrotgaulier@gmail.com>
2026-05-21 16:09:24 +0200

    Fixtures et FakerPHP

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ingredient;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Generator;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i <= 50; $i++) {
+            $ingredient = new Ingredient();
+            $ingredient->setName($this->faker->word())
+                ->setQuantity(mt_rand(1, 10) . ' units')
+                ->setPrice(mt_rand(1, 199));
+            $manager->persist($ingredient);
+        }
+
+        $manager->flush();
+    }
+}
--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\IngredientRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: IngredientRepository::class)]
 class Ingredient
@@ -14,16 +15,27 @@ class Ingredient
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $quantity = null;
 
     #[ORM\Column]
+    #[Assert\NotNull]
+    #[Assert\Positive]
+    #[Assert\LessThan(200)]
     private ?float $price = null;
 
     #[ORM\Column]
+    #[Assert\NotNull]
     private ?\DateTimeImmutable $createdAt = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
